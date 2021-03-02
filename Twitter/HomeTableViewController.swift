200c55b1a74b3191a803b7966fc67ffd5a1a9cc9
Mary Colleen Grace Meshach Ponraj <marycolleengrace@gmail.com>
2021-03-01 20:18:40 -0800

    Added a new feature to the app : User can compose a tweet and post it to the feed.

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -27,6 +27,10 @@ class HomeTableViewController: UITableViewController {
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
     }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
     
     @objc func loadTweets(){
         numberOfTweets = 20
